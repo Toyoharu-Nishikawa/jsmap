d6cf783d082f0ee6a0a09046851515421cb715f9
toyoharu nishikawa <toyohal@email.com>
2018-08-16 13:49:35 +0900

    add register

--- public/scripts/jsmap/view.js
+++ public/scripts/jsmap/view.js
@@ -2,17 +2,13 @@
 
 export const view = {
   elements: {
-    left: document.getElementById("left"),
-    center: document.getElementById("center"),
-    right: document.getElementById("right"),
-    input: document.getElementById("input"),
-    output: document.getElementById("output"),
-    read: document.getElementById("read"),
-    readInput: document.getElementById("readInput"),
-    saveInput: document.getElementById("saveInput"),
-    saveOutput: document.getElementById("saveOutput"),
-    sendData: document.getElementById("sendData"),
-    urlInput: document.getElementById("urlInput"),
-    viaserver: document.getElementById("viaserver"),
+    body: document.body,
+    aside: document.querySelector("aside"),
+    main: document.querySelector("main"),
+    expandbutton: document.getElementById("expandbutton"),
+    map: document.getElementById("map"),
+    markdown: document.getElementById("markdown"),
+    markdownArea: document.getElementById("markdownArea"),
+    registerbutton: document.getElementById("registerbutton"),
   }
 }
