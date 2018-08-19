ff68aaac4721a3bc37d9cff2d2787cbc9fcdb777
toyoharu nishikawa <toyohal@email.com>
2018-08-19 23:49:59 +0900

    add mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId
 const app = express();
 const fs = require('fs')
 const assert = require('assert')
+
 
 const log = require('log');
 
@@ -14,6 +17,7 @@ catch(err){
 }
 const logger = new log("info", fs.createWriteStream("/var/log/node/node.access.log",{flags:"a"}));
 
+const mongoURL = 'mongodb://mongo:27017';
 app.use(bodyParser.urlencoded({
         extended: true
 }));
@@ -23,56 +27,104 @@ app.set('port', 5000);
 app.use(express.static(__dirname + '/public'));
 
 app.all('/node', function(request, response) {
-  response.json({status:"jsonee server is working"})
-});
-
-app.all('/node/mirror', function(request, response) {
-  response.json(request.body)
+  response.json({status:"jsmap server is working"})
 });
 
 
-app.all('/node/server',(request,response)=>{
+app.all('/node/upload',(request,response)=>{
   const param = request.body
-  console.log(param)
-  if(param.hasOwnProperty("jsoneeSendURL")){
-    const url = param.jsoneeSendURL
-    console.log(`send to ${url}`)
-    logger.info(`send to ${url}`)
-    delete param.jsoneeSendURL 
+  const id = param.id
+  const longitude = parseFloat(param.longitude)
+  const latitude = parseFloat(param.latitude)
+  const title = param.title
+  const keywords = param.keywords.split(",").map(v=>v.trim())
+  const text = param.text
 
-    const data ={
-      body:JSON.stringify(param),
-      headers: {
-        'user-agent': 'Mozilla/4.0 MDN Example',
-        'content-type': 'application/json'
-      },
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  const wiki = {
+    longitude: longitude,
+    latitude: latitude,
+    title: title,
+    keywords: keywords,
+    text: text
+  }
+
+  let client = null
+  const main = async ()=>{
+    try{
+      client = await MongoClient.connect(mongoURL)
+      const db = client.db("map")
+      const collection = db.collection("wiki");
+      console.log("id",id)
+      if(id){
+        const result = await collection.update(
+            {_id:ObjectId(id)},
+            {$set:wiki},
+            {upsert:true}
+         )
+        response.json({register:true}) 
+      }
+      else {
+        const result = await collection.insert(wiki)
+        response.json({register:true, id:result.insertedIds[0]}) 
+      }
     }
-    const send = async() =>{
-      try{
-        const res = await fetch(url,data)
-        console.log(response.status)
-        const json = await res.json()
-        response.json(json)
-      }
-      catch(e){
-        console.log(e.message)
-        const json = {
-          error: e.message
-        }
-        response.json(json)
-      }
+    catch(e){
+      response.json({register:false, message:e.message})
+    }
+    finally{
+      client.close()
     }
   }
-  else{
-    console.log("error:the key of jsoneeSendURL is reserved for jsonee's server")
-    logger.info("error:the key of jsoneeSendURL is reserved for jsonee's server")
-    response.json({
-      error:"the key of jsoneeSendURL is reserved for jsonee's server"
-    })
+  main()
+}) 
+
+app.all('/node/initial',(request,response)=>{
+  let client = null
+  const main = async ()=>{
+    try{
+      client = await MongoClient.connect(mongoURL)
+      const db = client.db("map")
+      const collection = db.collection("wiki");
+      const result = await collection.find({},
+          {projection:{title:1, _id: 1,longitude: 1, latitude:1}})
+        .toArray()
+     response.json({result:result })
+    }
+    catch(e){
+      response.json({result:false, message:e.message})
+    }
+    finally{
+      client.close()
+    }
   }
+  main()
 })
+ 
+app.all('/node/getone',(request,response)=>{
+  const param = request.body
+  const id = param.id
 
+  let client = null
+  const main = async ()=>{
+    try{
+      client = await MongoClient.connect(mongoURL)
+      const db = client.db("map")
+      const collection = db.collection("wiki");
+      const result = await collection.findOne({_id:ObjectId(id)},
+          {projection:{title:1, longitude: 1, latitude:1, text:1, keywords:1}})
+ 
+      response.json({result:result })
+    }
+    catch(e){
+      response.json({result:false, message:e.message})
+    }
+    finally{
+      client.close()
+    }
+  }
+  main()
+})
+ 
 
 app.listen(app.get('port'), function() {
       console.log("Node app is running at localhost:" + app.get('port'))
